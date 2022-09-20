a89158273e3ba3ac8733d03e92a51de2f24d65a9
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2022-09-20 14:50:04 -0300

    test: add and find user an check it existence.

--- src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -8,4 +8,14 @@ describe('In memory user repository', () => {
     const user = await userRepo.findUserByEmail('any@email.com')
     expect(user).toBeNull()
   })
+
+  test('should return user if found in the repository', async () => {
+    const users: UserData[] = []
+    const name = 'any_name'
+    const email = 'any@email.com'
+    const userRepo = new InMemoryUserRepository(users)
+    await userRepo.add({ name, email })
+    const user = await userRepo.findUserByEmail('any@email.com')
+    expect(user.name).toBe('any_name')
+  })
 })
