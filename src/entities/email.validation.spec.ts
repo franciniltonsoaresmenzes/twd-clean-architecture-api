6fa56a346de1ef52b46fd22e9f996ddf7a85bb2d
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2022-09-20 17:15:04 -0300

    test: validate emails with empty local part

--- src/entities/email.validation.spec.ts
+++ src/entities/email.validation.spec.ts
@@ -30,4 +30,9 @@ describe('Email validation', () => {
     const email = 'l'.repeat(65) + '@email.com'
     expect(Email.validation(email)).toBeFalsy()
   })
+
+  test('should not accept local part', () => {
+    const email = '@email.com'
+    expect(Email.validation(email)).toBeFalsy()
+  })
 })
