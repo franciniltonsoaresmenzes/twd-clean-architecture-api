039895e88df39976fb3d2a774f0f162ccb6a9e41
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2022-09-20 16:47:21 -0300

    test: validate null and  empty emails.

---
+++ src/entities/email.validation.spec.ts
@@ -0,0 +1,18 @@
+import { Email } from './email'
+
+describe('Email validation', () => {
+  test('should not accept null string', () => {
+    const email = null
+    expect(Email.validation(email)).toBeFalsy()
+  })
+
+  test('should not accept empty string', () => {
+    const email = ''
+    expect(Email.validation(email)).toBeFalsy()
+  })
+
+  test('should accept valid emial', () => {
+    const email = 'any@email.com'
+    expect(Email.validation(email)).toBeTruthy()
+  })
+})
