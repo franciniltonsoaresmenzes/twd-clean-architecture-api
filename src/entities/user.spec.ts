ef91a2ab7d629007e0a54b3d9d3234f7378a43b3
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2022-09-22 15:45:51 -0300

    test: create valid user

--- src/entities/user.spec.ts
+++ src/entities/user.spec.ts
@@ -21,4 +21,13 @@ describe('User domain entites', () => {
     const error = User.create({ name: invalidName, email: 'any@email.com' })
     expect(error).toEqual(left(new InvalidNameError()))
   })
+
+  test('should create user with valid data', () => {
+    const validName = 'any_name'
+    const validEmail = 'any@email.com'
+    const user:User = User.create({ name: validName, email: validEmail }).value as User
+
+    expect(user.name.value).toEqual(validName)
+    expect(user.email.value).toEqual(validEmail)
+  })
 })
