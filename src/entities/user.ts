d15092a9f9eb58371c0956e69099611d76f991a0
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2022-09-22 15:45:37 -0300

    feat: create valid user

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,4 +1,4 @@
-import { Either, left } from '../shared/either'
+import { Either, left, right } from '../shared/either'
 import { Email } from './email'
 import { InvalidEmailError } from './errors/invalid-email-error'
 import { InvalidNameError } from './errors/invalid-name-error'
@@ -6,6 +6,14 @@ import { Name } from './name'
 import { UserData } from './use-data'
 
 export class User {
+  public readonly name:Name
+  public readonly email:Email
+
+  private constructor (name:Name, email:Email) {
+    this.name = name
+    this.email = email
+  }
+
   static create (userData: UserData): Either<InvalidNameError | InvalidEmailError, User> {
     const nameOrError = Name.create(userData.name)
 
@@ -17,5 +25,10 @@ export class User {
     if (emailOrError.isLeft()) {
       return left(new InvalidEmailError())
     }
+
+    const name: Name = nameOrError.value as Name
+    const email: Email = emailOrError.value as Email
+
+    return right(new User(name, email))
   }
 }
