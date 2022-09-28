36acd817d5378328870b1a88f711196bbbfbc97a
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2022-09-28 20:27:13 -0300

    test: config register route

---
+++ test/main/config/routes/register-route.test.ts
@@ -0,0 +1,16 @@
+import request from 'supertest'
+import app from '@/main/config/app'
+
+describe('Register route', () => {
+  test('should return an account on sucess', async () => {
+    app.post('/test_cors', (req, res) => res.send())
+
+    await request(app)
+      .post('/api/register')
+      .send({
+        name: 'Any name',
+        email: 'any@mail.com'
+      })
+      .expect(201)
+  })
+})
