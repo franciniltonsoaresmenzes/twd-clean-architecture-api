a30c616d133fafb20ebdfd837ed0d760fab6f3be
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2022-10-01 21:17:12 -0300

    chore: add start script

--- src/main/factories/register.ts
+++ src/main/factories/register.ts
@@ -1,6 +1,6 @@
 import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list'
 import { RegisterUserController } from '@/web-controllers'
-import { InMemoryUserRepository } from '@test/usecases/register-user-on-mailing-list/repository'
+import { InMemoryUserRepository } from '@/usecases/register-user-on-mailing-list/repository'
 
 export const makeRegisterUserController = (): RegisterUserController => {
   const inMemoryUserRepository = new InMemoryUserRepository([])
--- test/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
+++ test/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -1,5 +1,5 @@
 import { UserData } from '@/entities'
-import { InMemoryUserRepository } from '@test/usecases/register-user-on-mailing-list/repository'
+import { InMemoryUserRepository } from '@/usecases/register-user-on-mailing-list/repository'
 
 describe('In memory user repository', () => {
   test('should return null will if user is not found', async () => {
