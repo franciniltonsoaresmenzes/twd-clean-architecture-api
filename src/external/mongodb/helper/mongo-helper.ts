c35fc73568e3c413a9cb273b586a9dfc5f57c779
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2022-10-05 15:53:17 -0300

    chore: user add and verify using mongodb

--- src/external/mongodb/helper/mongo-helper.ts
+++ src/external/mongodb/helper/mongo-helper.ts
@@ -3,19 +3,18 @@ import { Collection, MongoClient } from 'mongodb'
 export const MongoHelper = {
   cliente: null as MongoClient,
   async connect (uri: string): Promise<void> {
-    this.client = new MongoClient(uri)
-    this.client.connect(uri, {
-      proxyUsername: true,
+    this.client = await MongoClient.connect(uri, {
+      useNewUrlParser: true,
       useUnifiedTopology: true
     })
   },
   async disconnect (): Promise<void> {
-    this.cliente.close()
+    this.client.close()
   },
   getCollection (name: string): Collection {
-    return this.cliente.db().collection(name)
+    return this.client.db().collection(name)
   },
-  clearCollection (name: string): void {
-    this.client.db().collection(name).deleteMany()
+  async clearCollection (name: string): Promise<void> {
+    this.client.db().collection(name).deleteMany({})
   }
 }
---
+++ src/external/mongodb/mongodb-user-repository.ts
@@ -0,0 +1,33 @@
+import { UserData } from '@/entities'
+import { UserRepository } from '@/usecases/register-user-on-mailing-list/ports'
+import { MongoHelper } from './helper'
+
+export class MongodbUserRepository implements UserRepository {
+  async add (user: UserData): Promise<void> {
+    const userCollection = MongoHelper.getCollection('users')
+    const exists = await this.exist(user)
+
+    if (!exists) {
+      await userCollection.insertOne(user)
+    }
+  }
+
+  async findUserByEmail (email: string): Promise<UserData> {
+    const userCollection = MongoHelper.getCollection('users')
+    const result = await userCollection.findOne({ email })
+    return result
+  }
+
+  async findAllUser (): Promise<UserData[]> {
+    throw new Error('not implements')
+  }
+
+  async exist (user: UserData): Promise<boolean> {
+    const result = await this.findUserByEmail(user.email)
+    if (result != null) {
+      return true
+    }
+
+    return false
+  }
+}
