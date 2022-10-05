8e449eafa8e2a8f480713f6336f9aa71fef2c2bc
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2022-10-05 16:35:24 -0300

    refactor: iMemory

--- src/main/factories/register.ts
+++ src/main/factories/register.ts
@@ -1,9 +1,11 @@
 import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list'
 import { RegisterUserController } from '@/web-controllers'
-import { MongodbUserRepository } from '@/external/mongodb'
+// import { MongodbUserRepository } from '@/external/mongodb'
+import { InMemoryUserRepository } from '@/usecases/register-user-on-mailing-list/repository'
 
 export const makeRegisterUserController = (): RegisterUserController => {
-  const mongoDbUserRepository = new MongodbUserRepository()
+  // const mongoDbUserRepository = new MongodbUserRepository()
+  const mongoDbUserRepository = new InMemoryUserRepository([])
   const registerUserOnMailingListUseCase = new RegisterUserOnMailingList(mongoDbUserRepository)
   const registerUserController = new RegisterUserController(registerUserOnMailingListUseCase)
   return registerUserController
