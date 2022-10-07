f37e8747fc620edaf8c58231eaef4abc90ea3a9a
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2022-10-06 22:15:37 -0300

    refact: consert my error

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,7 +1,7 @@
-import { MongoHelper } from '@/external/mongodb/helper'
 import 'module-alias/register'
+import { MongoHelper } from '@/external/repositories/mongodb/helper'
 
-MongoHelper.connect(process.env.MONGO_URL)
+MongoHelper.connect('mongodb://localhost:27017')
   .then(async () => {
     const app = (await import('./config/app')).default
     app.listen(5000, () => {
