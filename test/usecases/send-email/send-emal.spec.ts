419c4702c49532ff8eb4171399443954c2bc3a64
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2022-10-11 18:58:08 -0300

    test: send email when name and email are valid

---
+++ test/usecases/send-email/send-emal.spec.ts
@@ -0,0 +1,46 @@
+import { Either, right, Right } from '@/shared'
+import { MailServiceError } from '@/usecases/error/mail-service-error'
+import { EmailOptions, EmailService } from '@/usecases/send-email/ports/email-service'
+import { SendEmail } from '@/usecases/send-email/send-email'
+
+const attachmentFilePath = '../resources/text.txt'
+const fromName = 'Test'
+const fromEmail = 'from_any@mail.com'
+const toName = 'any_name'
+const toEmail = 'any_email@mail.com'
+const subject = 'Test e-mail'
+const emailBody = 'Hello world attachment test'
+const emailBodyHtml = '<b>Hello world attachment test</b>'
+const attachment = [{
+  filename: attachmentFilePath,
+  contextType: 'text/plain'
+}]
+
+const mailOptions: EmailOptions = {
+  host: 'test',
+  port: 867,
+  username: 'test',
+  password: 'test',
+  from: fromName + ' ' + fromEmail,
+  to: toName + '<' + toEmail + '>',
+  subject,
+  text: emailBody,
+  html: emailBodyHtml,
+  attachment
+}
+
+class MailServiceStub implements EmailService {
+  async send (options: EmailOptions): Promise<Either<MailServiceError, EmailOptions>> {
+    return right(options)
+  }
+}
+
+describe('Send email to user', () => {
+  it('should email user with valid name and email address', async () => {
+    const mailServiceStub = new MailServiceStub()
+    const useCase = new SendEmail(mailOptions, mailServiceStub)
+    const response = await useCase.perform({ name: toName, email: toEmail })
+
+    expect(response).toBeInstanceOf(Right)
+  })
+})
