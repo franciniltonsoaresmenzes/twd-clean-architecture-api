e6f956499b2106c594ca71fca098857901f72926
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2022-10-20 20:44:19 -0300

    feat: handle invalid user data

--- src/usecases/send-email/send-email.ts
+++ src/usecases/send-email/send-email.ts
@@ -1,8 +1,9 @@
-import { UserData } from '@/entities'
-import { Either } from '@/shared'
+import { User, UserData } from '@/entities'
+import { Either, left } from '@/shared'
 import { UseCase } from '@/usecases/ports'
 import { MailServiceError } from '../error/mail-service-error'
 import { EmailOptions, EmailService } from '@/usecases/send-email/ports/email-service'
+import { InvalidEmailError, InvalidNameError } from '@/entities/errors'
 
 export class SendEmail implements UseCase {
   private readonly emailOptions: EmailOptions
@@ -13,12 +14,21 @@ export class SendEmail implements UseCase {
     this.emailService = emailService
   }
 
-  async perform (userData: UserData): Promise<Either<MailServiceError, EmailOptions>> {
-    const greetings = 'E aí <b>' + userData.name + '</b>, beleza?'
+  async perform (userData: UserData):
+    Promise<Either<InvalidNameError | InvalidEmailError | MailServiceError, EmailOptions>> {
+    const userOrError: Either<InvalidNameError | InvalidEmailError, User> = User.create(userData)
+
+    if (userOrError.isLeft()) {
+      return left(userOrError.value)
+    }
+
+    const user = userOrError.value
+
+    const greetings = 'E aí <b>' + user.name + '</b>, beleza?'
     const customizedHtml = greetings + '<br><br>' + this.emailOptions.html
     const emailInfo: EmailOptions = {
       ...this.emailOptions,
-      to: userData.name + '<' + userData.email + '>',
+      to: user.name + '<' + user.email + '>',
       html: customizedHtml
     }
 
