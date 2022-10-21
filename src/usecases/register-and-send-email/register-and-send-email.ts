de08d12d74257ed9b87c9dd365e37872890df840
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2022-10-20 21:54:49 -0300

    feat: register and send email

---
+++ src/usecases/register-and-send-email/register-and-send-email.ts
@@ -0,0 +1,37 @@
+import { User, UserData } from '@/entities'
+import { Either, left, right } from '@/shared'
+import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list'
+import { SendEmail } from '@/usecases/send-email/send-email'
+import { UseCase } from '@/usecases/ports'
+import { InvalidEmailError, InvalidNameError } from '@/entities/errors'
+import { MailServiceError } from '@/usecases/error/mail-service-error'
+
+export class RegisterAndSendEmail implements UseCase {
+  private registerUserOnMailingList: RegisterUserOnMailingList
+  private sendEmail: SendEmail
+
+  constructor (registerUserOnMailingList: RegisterUserOnMailingList, sendEmail: SendEmail) {
+    this.registerUserOnMailingList = registerUserOnMailingList
+    this.sendEmail = sendEmail
+  }
+
+  async perform (request: UserData): Promise<Either<InvalidNameError | InvalidEmailError | MailServiceError, User>> {
+    const userOrError: Either<InvalidNameError | InvalidEmailError, User> = User.create(request)
+
+    if (userOrError.isLeft()) {
+      return left(userOrError.value)
+    }
+
+    const user = userOrError.value
+    const userData = { name: user.name.value, email: user.email.value }
+
+    await this.registerUserOnMailingList.perform(userData)
+    const result = await this.sendEmail.perform(userData)
+
+    if (result.isLeft()) {
+      return left(result.value)
+    }
+
+    return right(user)
+  }
+}
